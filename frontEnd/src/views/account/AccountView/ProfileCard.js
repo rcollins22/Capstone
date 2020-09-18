96cf0beee74ce2b6c1b08072d071934deb14a238
Rashad Collins <rashad.collins22@gmail.com>
2020-09-18 14:49:39 -0400

    update

--- frontEnd/src/views/account/AccountView/ProfileCard.js
+++ frontEnd/src/views/account/AccountView/ProfileCard.js
@@ -21,7 +21,7 @@ const user = {
   leader: true,
   name: 'Rashad Collins',
   timezone: 'GTM-7'
-}
+};
 
 const useStyles = makeStyles(() => ({
   root: {},
@@ -31,35 +31,18 @@ const useStyles = makeStyles(() => ({
   }
 }));
 
-const Profile = ({ className, ...rest }) => {
+const ProfileCard = ({ className, ...rest }) => {
   const classes = useStyles();
 
   return (
-    <Card
-      className={clsx(classes.root, className)}
-      {...rest}
-    >
+    <Card className={clsx(classes.root, className)} {...rest}>
       <CardContent>
-        <Box
-          alignItems="center"
-          display="flex"
-          flexDirection="column"
-        >
-          <Avatar
-            className={classes.avatar}
-            src={user.avatar}
-          />
-          <Typography
-            color="textPrimary"
-            gutterBottom
-            variant="h3"
-          >
+        <Box alignItems="center" display="flex" flexDirection="column">
+          <Avatar className={classes.avatar} src={user.avatar} />
+          <Typography color="textPrimary" gutterBottom variant="h3">
             {user.name}
           </Typography>
-          <Typography
-            color="textSecondary"
-            variant="body1"
-          >
+          <Typography color="textSecondary" variant="body1">
             {`${user.city} ${user.country}`}
           </Typography>
           <Typography
@@ -73,11 +56,7 @@ const Profile = ({ className, ...rest }) => {
       </CardContent>
       <Divider />
       <CardActions>
-        <Button
-          color="primary"
-          fullWidth
-          variant="text"
-        >
+        <Button color="primary" fullWidth variant="text">
           Upload picture
         </Button>
       </CardActions>
@@ -85,8 +64,8 @@ const Profile = ({ className, ...rest }) => {
   );
 };
 
-Profile.propTypes = {
+ProfileCard.propTypes = {
   className: PropTypes.string
 };
 
-export default Profile;
+export default ProfileCard;

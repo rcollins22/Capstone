f8710a3db0d4babe13ff8d92e9a9c0f48d7952fb
David Mitchell <dmitchell217@gmail.com>
2020-09-20 13:05:24 -0400

    new branch

--- backEnd/src/action/portfolio/sellStock.js
+++
@@ -1,8 +0,0 @@
-// sell a stock
-    // determine max volume of stock possible to sell
-    // grab user input from front end
-    // calculate portfolio allocation reduction resulting from sale
-    // delete stock from stock table
-    // update portfolio table
-        // allocation percentage
-    // re-render front end to showcase purchase
